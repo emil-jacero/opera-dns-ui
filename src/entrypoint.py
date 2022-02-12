f5744c06a34de63372bc6398e3cb71c0c4b8b64d
Emil Larsson <emil@jacero.se>
2022-02-12 19:08:41 +0100

    Update

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -4,6 +4,7 @@ from email.policy import default
 from multiprocessing.connection import wait
 import os
 import sys
+import psycopg2
 import subprocess
 import requests
 import json
@@ -11,7 +12,6 @@ import time
 
 from lib.logger import logger as log
 from lib.template import Template
-from lib.gpgsql import wait_for_db, has_existing_default_user, execute_sql
 
 # Set working directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -22,12 +22,14 @@ template_path = os.path.join(base_dir, 'templates')
 config_ini_path = "/srv/dns-ui/config/config.ini"
 ssmtp_path = "/etc/ssmtp/ssmtp.conf"
 insert_user_sql_path = "/app/insert_user.sql"
+migration_002_path = "/srv/dns-ui/migrations/002.php"
 
 if os.getenv('DEV') == "true":
     template_path = os.path.join(base_dir, 'templates')
     config_ini_path = f"{base_dir}/dev/config.ini"
     ssmtp_path = f"{base_dir}/dev/ssmtp.conf"
     insert_user_sql_path = f"{base_dir}/dev/insert_user.sql"
+    migration_002_path = f"{base_dir}/dev/002.php"
 
 
 log.debug(f"base_dir: {base_dir}")
@@ -42,13 +44,39 @@ renderer = Template()
 def get_from_environment(env_search_terms=["ENV_"]):
     enviroment = {}
     log.info("Getting environment variables")
-    for search_term in env_search_terms:
+    for term in env_search_terms:
         for k, v in os.environ.items():
-            if f"{search_term}" in k:
+            if f"{term}" in k:
                 obj = {k: v}
                 enviroment.update(obj)
     log.debug(json.dumps(enviroment, indent=2))
     return enviroment
+
+
+def db_connect_check(host, port, user, password):
+    conn = None
+    try:
+        conn_string = f"host={host} port={port} user={user} password={password} connect_timeout=1"
+        conn = psycopg2.connect(conn_string)
+        return True
+    except:
+        return False
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def wait_for_db(host, port, user, password, timeout=30):
+    time_end = time.time() + timeout
+    while db_connect_check(host, port, user, password) is False:
+        if time.time() > time_end:
+            log.error('Could not connect to the database')
+            sys.exit(1)
+        log.info(
+            f"Waiting for postgres at: {host}:{port}"
+        )
+        time.sleep(5)
+    log.info(f"Successfully connected to database ({host}:{port})")
 
 
 def web_connect_check(url, headers):
@@ -76,6 +104,26 @@ def wait_for_api(url, headers, timeout=30):
     log.info(f"Successfully connected to PowerDNS API ({url})")
 
 
+def insert_default_user(migration_path, default_user, data):
+    with open(migration_path) as f:
+        if not 'INSERT INTO "user"' in f.read():
+            log.info(f"Inserting default user ({default_user})")
+            orig_file = open(migration_path, 'r')
+            file_list = list(orig_file)
+            count = 1
+            for line in reversed(file_list):
+                if line == "}" or line == "}\n":
+                    file_list = (file_list[:-count]) + data
+                    new_file = open(migration_path, 'w')
+                    for line in file_list:
+                        new_file.write(line)
+                    orig_file.close()
+                    new_file.close()
+                count = count + 1
+        else:
+            log.info(f"Already found insert for ({default_user})")
+
+
 def main():
     # Write templates
     render_list = [{"template": "config.ini.j2",
@@ -101,12 +149,17 @@ def main():
     psql_user = os.getenv('POSTGRES_USER', default="dnsui")
     psql_password = os.getenv('POSTGRES_PASSWORD', default="CHANGEME")
     default_user = os.getenv('ADMIN_USER', default="admin")
+    insert_data = [f"""
+\t$this->database->prepare('
+\tINSERT INTO "user" (uid, name, email, active, admin, auth_realm)
+\t\tVALUES (?, ?, ?, ?, ?, ?)
+\t')->execute(
+\t\tarray("{default_user}", "Example User", "{default_user}@example.com", 1, 1, "local")
+\t);
+}}"""]
 
     wait_for_db(psql_host, psql_port, psql_user, psql_password)
-    if not has_existing_default_user(psql_host, psql_port, psql_user, psql_password, psql_db, default_user):
-        log.info("Insert default User ({})")
-        execute_sql(psql_host, psql_port, psql_user,
-                    psql_password, psql_db, insert_user_sql_path)
+    insert_default_user(migration_002_path, default_user, insert_data)
 
     # Only continue if connection to API succeeds
     api_protocol = os.getenv('DNSUI_API_PROTOCOL', default="http")
